aede4cec18f8ce8f9bc6afa5a1718b6f60a35fb7
wolfitdm <wolfitdm@gmail.com>
2026-03-28 11:06:07 +0100

    update

--- CheatMenuPlugin/js/main2.js
+++ CheatMenuPlugin/js/main2.js
@@ -6,5 +6,5 @@ window.onload = function() {
     $plugins.push({"name":"Cheat_Menu","status":true,"description":"","parameters":{}});
     PluginManager.loadScript('../../cheat/init/import.js');
     PluginManager.setup($plugins);
-    //SceneManager.run(Scene_Boot);
+    SceneManager.run(Scene_Boot);
 };
