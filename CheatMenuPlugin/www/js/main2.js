5fb0a5f549f6d282a61cf800ffdc3aa48d02d6a3
wolfitdm <wolfitdm@gmail.com>
2026-03-30 14:17:28 +0200

    update

--- CheatMenuPlugin/www/js/main2.js
+++ CheatMenuPlugin/www/js/main2.js
@@ -2,9 +2,23 @@
 // main2.js
 //=============================================================================
 
-window.onload = function() {
+/*window.onload = function() {
     $plugins.push({"name":"Cheat_Menu","status":true,"description":"","parameters":{}});
     PluginManager.loadScript('../../cheat/init/import.js');
     PluginManager.setup($plugins);
     SceneManager.run(Scene_Boot);
-};
+};*/
+
+function loadPlugin(pluginName, path) {
+    try {
+        var script = document.createElement('script');
+        script.src = path + "plugins/" + pluginName + ".js";
+        document.body.appendChild(script);
+    } catch (error_ex) {
+    } 
+}
+
+window.addEventListener("load", function() {
+    PluginManager.loadScript('../../cheat/init/import.js');
+    loadPlugin('Cheat_Menu', "js/");
+});
